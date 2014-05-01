0a0b7a3194bde5710d04a30cf1738667df3cddc9
Jordan Balls <jordan_balls@yahoo.com>
2014-05-01 05:36:39 -0600

    adding image

--- index.php
+++ index.php
@@ -7,12 +7,17 @@
   <title>Welcome to Jordan Balls' Homepage!</title>
 
 <body>
+
  <h1>Jordan Balls CS313 Home Page</h1>
+
  <div class="links">
   <a href="index.php">Home</a><br/>
   <a href="assignments.html">Assignments</a>
  </div>
 
+ <div class="right">
+  <img id="self" src="main.jpg" alt="Me!" width="300" height="right300"/>
+ </div>
 
 
 </body>
