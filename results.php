0a249302bce7d30a4e8ba9cb9a3a5b5b7ed65bb8
Jordan Balls <jordan_balls@yahoo.com>
2014-05-12 14:57:00 -0600

    some bugs worked out

--- results.php
+++ results.php
@@ -1,6 +1,69 @@
 <?php
-   echo $_POST['major'], '<br/>';
-   echo $_POST['os'], '<br/>';
-   echo $_POST['grade'], '<br/>';
-   echo $_POST['internship'], '<br/>';
+session_start();
+$major = $_POST['major'];
+$os = $_POST['os'];
+$grade = $_POST['grade'];
+$internship = $_POST['internship'];
+
+if(!isset($_SESSION['done']));
+{
+$fp = fopen("formdata.txt", "a");
+$savestring = $major . ", " . $os . ", " . $grade . ", " . $internship . "\r\n";
+fwrite($fp, $savestring);
+fclose($fp);
+$_SESSION['done']=1;
+}
 ?>
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <link rel="stylesheet" type="text/css" href="styles.css"/>
+  <title>Welcome to Jordan Balls' Homepage!</title>
+</head>
+
+<body>
+ <h1>Results Page!</h1>
+ <div class="links">
+  <a href="index.php">Home</a><br/>
+  <a href="assignments.html">Assignments</a>
+ </div>
+
+<div class="main">
+<?php
+   echo "Your Major: ", $major, '<br/>';
+   echo "Your Preferred OS: ", $os, '<br/>';
+   echo "Your Grade Level: ", $grade, '<br/>';
+   echo "Done Internship?: ", $internship, '<br/>';
+?>
+<br/><br/>
+
+Results<br/><br/>
+
+<?php
+$text = file_get_contents("formdata.txt");
+
+echo "Majors: ", substr_count($text, 'CS'), " CS--",
+     substr_count($text, 'CE'), " CE--",
+     substr_count($text, 'CIT'), " CIT--",
+     substr_count($text, 'Web'), " Web--",
+     substr_count($text, 'Other'), " Other<br/>";
+
+echo "OS: ", substr_count($text, 'Windows'), " Windows--",
+     substr_count($text, 'Mac'), " Mac--",
+     substr_count($text, 'Linux'), " Linux--",
+     substr_count($text, 'Other'), " Other<br/>";
+
+echo "Grade Level: ", substr_count($text, 'Freshman'), " Freshman--",
+     substr_count($text, 'Sophomore'), " Sophomore--",
+     substr_count($text, 'Junior'), " Junior--",
+     substr_count($text, 'Senior'), " Senior<br/>";
+
+echo "Internships: ", substr_count($text, 'Yes'), " Completed--",
+     substr_count($text, 'No'), " Have Not<br/>";
+?>
+
+</div>
+</body>
+
+</html>
--- survey.php
+++ survey.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+if(isset($_SESSION['views']))
+{
+header("location: results.php");
+}
+else
+$_SESSION['views']=1;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,32 +30,38 @@
  <div class="main">
  <form action="results.php" method="post">
  What is your major?<br/>
- CS     <input type="radio" name="major" value="CS">
+ CS     <input type="radio" name="major" value="CS" checked>
  CE     <input type="radio" name="major" value="CE">
  CIT    <input type="radio" name="major" value="CIT">
  Web    <input type="radio" name="major" value="Web">
  Other  <input type="radio" name="major" value="Other"><br/><br/>
  
  Preferred OS?<br/>
- Windows<input type="radio" name="os" value="Windows">
+ Windows<input type="radio" name="os" value="Windows" checked>
  Mac    <input type="radio" name="os" value="Mac">
  Linux  <input type="radio" name="os" value="Linux">
  Other  <input type="radio" name="os" value="Other"><br/><br/>
 
  How far into your coursework are you?<br/>
- Freshman   <input type="radio" name="grade" value="Freshman">
+ Freshman   <input type="radio" name="grade" value="Freshman" checked>
  Sophomore  <input type="radio" name="grade" value="Sophomore">
  Junior     <input type="radio" name="grade" value="Junior">
  Senior     <input type="radio" name="grade" value="Senior"><br/><br/>
 
  Have you completed an internship?<br/>
- Yes <input type="radio" name="internship" value="Yes">
+ Yes <input type="radio" name="internship" value="Yes" checked>
  No  <input type="radio" name="internship" value="No"><br/><br/>
 
  <input type="submit">
  </form>
  </div>
 
+<php
+$_SESSION['major'] = $_POST['major'];
+$_SESSION['os'] = $_POST['os'];
+$_SESSION['grade'] = $_POST['grade'];
+$_SESSION['internship'] = $_POST['internship'];
+?>
 
 </body>
 
