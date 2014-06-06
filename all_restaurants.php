ee71e3b7de848ab84b62d22292162a4dc17b7419
Jordan Balls <jordan_balls@yahoo.com>
2014-06-06 11:45:33 -0600

    fixing bug

--- all_restaurants.php
+++ all_restaurants.php
@@ -46,8 +46,7 @@ foreach($rows AS $row)
 echo '<b> Name: ' . '<a href="display_reviews.php?id=' . $row['id'] . '">' . $row['name'] . '</a>' 
      . "<br/> Address: " . $row['address'] . '</b>';
 	 if (isset($_SESSION['username']))
-	 echo '<form action="add_review.php" method="POST"><input type="button" value="Add Review"></form>';
-	 echo '<br/>';
+	 echo '<form action="add_review.php" method="POST"><input type="submit" value="Add Review"></form>';
 }
 echo '</div>';
 ?>
